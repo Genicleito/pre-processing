6586c676b9fd1ce02ba61350e07c14e0dc8d2d3a
Genicleito Gonçalves <genicleito@live.com>
2021-01-14 00:56:14 -0300

    :tada: new version

--- conversion/treatCSV.py
+++ conversion/treatCSV.py
@@ -1,3 +1,5 @@
+# OBS: considerar tbm casos: ", | ,"
+
 # Developed by: Genicleito Gonçalves
 
 # Function to handle CSV files by removing characters such as quotation marks, backslash, comma, semicolon.
@@ -12,27 +14,30 @@ def treatCSV(source, target, sep):
     Example: treatCSV('/home/user/source_file.csv', '/home/user/output_file.csv', sep = ",")
     """
     import os
-    
-    sep_aux = "_separator_aux_"
-    target_aux = target + ".part"
+    quote_ctrl = '_quote_control_'
+    field_separator = '_field_separator_'
+    # aux_sep = '_auxiliary_separator_'
+    aux_start_sep = '_auxiliary_start_separator_'
+    aux_end_sep = '_auxiliary_end_separator_'
+    # target_aux = target + ".part"
 
     # List of sed commands used to treat the dataset
-    seds = [
-        "sed -r 's/\\\"\\" + sep + "\\\"/" + sep_aux + "/g' " + source,      # replaces the separator with the auxiliary separator
+    sed_conditions = [
+        f"s/[\\]*\"{sep}\"/{field_separator}/g;s/[\\]*\"{sep}/{aux_end_sep}/g;s/[\\]*{sep}\"/{aux_start_sep}/g",    # replaces the separator with the auxiliary separator
         
         # Using the '+' character in regex has the function of obtaining one or more backslashes
-        "sed -r 's/\\\\+\\" + sep_aux + "/" + sep_aux + "/g'",     # remove backslashes before auxiliary separator
-        "sed -r 's/\\\\+\\\"//g'",                   # remove quotes with backslash
+        # f"s/[\\]+{field_separator}/{field_separator}/g",     # remove backslashes before auxiliary separator
+        # "s/[\\]+\"//g",                   # remove quotes with backslash
 
-        "sed -r 's/^\\\"|\"$/_control_quotes_/g'",    # save the quotation marks that mark the beginning and end of the record
-        "sed -r 's/\\\"//g'",                  # remove quotes without backslashes
+        f"s/^\"/{quote_ctrl}/g;s/\"$/{quote_ctrl}/g",    # save the quotation marks that mark the beginning and end of the record
+        "s/[\\]*\"//g",                  # remove quotes # without backslashes
         
-        "sed -r 's/\\" + sep_aux + "/\\\"\\,\\\"/g'",    # set the comma separator and restore quotation marks between fields
-        "sed -r 's/_control_quotes_/\\\"/g' > " + target      # restore quotation marks at beginning and end of record
+        f"s/{field_separator}/\",\"/g;s/{aux_start_sep}/{sep}\"/g;s/{aux_end_sep}/\"{sep}/g",    # set the comma separator and restore quotation marks between fields
+        f"s/{quote_ctrl}/\"/g"      # restore quotation marks at beginning and end of record
     ]
-    re_sed_commands = " | ".join(seds)
+    cmd = f'sed -r \'{";".join(sed_conditions)}\' {source} > {target}'
 
     # Apply the sed commands
-    if(os.system(re_sed_commands) != 0):
+    if(os.system(cmd) != 0):
         return False
     return True
