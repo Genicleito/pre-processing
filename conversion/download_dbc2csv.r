2d5c682e9322db6101ea455b5e3545a9003cb2a2
Genicleito Gonçalves <genicleito@live.com>
2021-01-25 17:26:28 -0300

    :tada: updated

--- conversion/download_dbc2csv.r
+++ conversion/download_dbc2csv.r
@@ -5,19 +5,31 @@ install.packages('read.dbc')
 require('read.dbc')
 require('curl')
 
-# Download files
-dir.create('download_files/')
-url = 'ftp://ftp.datasus.gov.br/dissemin/publicos/SINASC/NOV/DNRES/'
-for (ano in 2014:2017) {
-	filename = paste('DNBR', ano, '.dbc', sep = '')
-	cat('Downloading...', filename, '\n')
-	curl_download(url = paste(url, filename, sep = ''), destfile = paste('download_files/', filename, sep = ''))
+raw_path = 'download_files/raw/'
+refined_path = 'download_files/refined/'
+
+extract <- function(url = 'ftp://ftp.datasus.gov.br/dissemin/publicos/SINASC/NOV/DNRES/', base_name = '', write_path = raw_path, range_years = 2000:2019) {
+  # Download files
+  if (!dir.exists(write_path)) { dir.create(write_path, recursive = T) }
+  for (ano in range_years) {
+    filename = paste(base_name, ano, '.dbc', sep = '')
+    cat('Downloading...', filename, '\n')
+    curl_download(url = paste(url, filename, sep = ''), destfile = paste(write_path, filename, sep = ''))
+  }
 }
 
-# convert files to .csv
-bases = list.files('download_files/', pattern = '.dbc')
-for (base in bases) {
-  filename = paste('download_files', base, sep = '/')
-  cat('Converting to csv...', filename, '\n')
-  write.csv(x = read.dbc(filename), file = gsub('.dbc', '.csv', filename), row.names = F, na = "")
+conversion <- function(read_path = raw_path, write_path = refined_path) {
+  # create dir
+  if (!dir.exists(write_path)) { dir.create(write_path, recursive = T) }
+  # convert files to .csv
+  bases = list.files(read_path, pattern = '.dbc')
+  for (base in bases) {
+    filename = paste(read_path, base, sep = '')
+    csvFile = gsub('.dbc', '.csv', paste(write_path, base, sep = ''))
+    cat('Converting to csv...', filename, '\n')
+    write.csv(x = read.dbc(filename), file = csvFile, row.names = F, na = "")
+  }
 }
+
+extract(base_name = 'DNBR', range_years = 2014:2017)
+conversion()
