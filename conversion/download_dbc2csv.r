ef9472dab9dd62ff6d0364b28e277e5879367bd1
Genicleito Gonçalves <genicleito@live.com>
2021-01-28 21:39:47 -0300

    :up: new_version :tada:

--- conversion/download_dbc2csv.r
+++ conversion/download_dbc2csv.r
@@ -7,14 +7,38 @@ require('curl')
 
 raw_path = 'download_files/raw/'
 refined_path = 'download_files/refined/'
+url_ftp = 'ftp://ftp.datasus.gov.br/dissemin/publicos/SINASC/NOV/DNRES/'
 
-extract <- function(url = 'ftp://ftp.datasus.gov.br/dissemin/publicos/SINASC/NOV/DNRES/', base_name = '', write_path = raw_path, range_years = 2000:2019) {
+list_files_from_ftp <- function(url = url_ftp) {
+  # handle to list dir
+  h = new_handle(dirlistonly = T)
+  # create a connection with the url
+  con = curl(url = url_ftp, open = 'r', handle = h)
+  # get a list of the files
+  files = read.table(con)[, 1]
+  # build the complete URL of the files
+  f = function(x) { paste(url_ftp, x, sep = '') }
+  # returns a vector with the links of the listed files
+  unlist(lapply(files, f))
+}
+
+extract <- function(url = url_ftp, base_name = '', write_path = raw_path, range_years = NA) {
   # Download files
   if (!dir.exists(write_path)) { dir.create(write_path, recursive = T) }
-  for (ano in range_years) {
-    filename = paste(base_name, ano, '.dbc', sep = '')
-    cat('Downloading...', filename, '\n')
-    curl_download(url = paste(url, filename, sep = ''), destfile = paste(write_path, filename, sep = ''))
+  if (!is.na(range_years)) {
+    for (ano in range_years) {
+      filename = paste(base_name, ano, '.dbc', sep = '')
+      cat('Downloading...', filename, '\n')
+      curl_download(url = paste(url, filename, sep = ''), destfile = paste(write_path, filename, sep = ''))
+    }
+  } else {
+    bases = list_files_from_ftp(url)
+    for (base in bases) {
+      filename = tail(unlist(strsplit(base, '/')), n=1)
+      target_file = paste(write_path, gsub('.DBC', '.dbc', filename), sep = '')
+      cat('Downloading...', filename, '|', 'Saving in...', target_file, '\n')
+      curl_download(url = paste(url, filename, sep = ''), destfile = target_file)
+    }
   }
 }
 
